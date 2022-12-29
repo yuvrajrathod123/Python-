0e9b1602433294de6fd582ac1eccebe5524a33a2
yuvrajrathod123 <yuvirathod122@gmail.com>
2022-12-30 01:09:06 +0530

    commit

---
+++ If_else/E6.py
@@ -0,0 +1,9 @@
+
+# write a program to find whether a given number is even or odd and print the message accordrdingly
+
+n = int(input("Enter a number: "))
+
+if n % 2 == 0:
+    print("The number is even")
+else:
+    print("the number is odd")
---
+++ String/S1.py
@@ -0,0 +1,13 @@
+
+# program to convert this string "hello python" into uppercase using string in-built function
+
+a = "hello python"
+b = a.capitalize()
+print(b)
+
+# wriyte a program to convert this string "HELLO PYTHON" into lower case using string in-built fumction
+
+a = "HELLO PYTHON"
+b = a.casefold
+print(b)
+
