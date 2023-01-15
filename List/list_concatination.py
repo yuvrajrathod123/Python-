731cbed5e1f0eb0e22883751aae10e4ef5ade8e6
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-15 15:21:53 +0530

    list concatination

---
+++ List/list_concatination.py
@@ -0,0 +1,11 @@
+
+
+# List Concatenation
+# If you have to concatenate multiple lists, you can use the + operator. This will create a new list, and the original lists will remain unchanged.
+
+evens = [2, 4, 6]
+odds = [1, 3, 5]
+
+nums = odds + evens
+
+print(nums)  # [1, 3, 5, 2, 4, 6]
