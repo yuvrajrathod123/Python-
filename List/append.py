d752328a1811d18264186e83760605b471e41bfe
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-15 14:55:52 +0530

    append method

---
+++ List/append.py
@@ -0,0 +1,14 @@
+
+
+# append()
+# This function adds a single element to the end of the list
+
+fruit = ['apple','banana','orange','kiwi']
+
+print("The current list:",fruit)
+
+new_fruit = input("Enter the fruit to be add at last:")
+
+fruit.append(new_fruit)
+
+print("Updated list:",fruit)
