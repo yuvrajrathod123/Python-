a438ab88445690924e648bb7b1ff41c2e8056b42
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-16 22:20:47 +0530

    how to use else part in exception

---
+++ Exception_Handling/p3.py
@@ -0,0 +1,21 @@
+
+
+# how to use else part in exception
+
+# try:
+#     print("hello guys")
+# except:
+#     print("something went wrong")
+# else:
+#     print("Nothing went wrong")        
+
+
+# now lets re write the code with an acception    
+
+
+try:
+    print(x)
+except:
+    print("something went wrong")
+else:
+    print("Nothing went wrong")        
