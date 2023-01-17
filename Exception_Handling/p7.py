56d431dfdf57e933af54139f54f8a02ed8ceeaa7
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-17 22:45:34 +0530

    use multiple exception block

---
+++ Exception_Handling/p7.py
@@ -0,0 +1,14 @@
+
+#  use multiple exception block
+try:
+    
+    even_numbers = [2,4,6,8]
+    print(even_numbers[5])
+
+except ZeroDivisionError:
+    print("Denominator cannot be 0.")
+    
+except IndexError:
+    print("Index Out of Bound.")
+
+# Output: Index Out of Bound
