2edfd5b8bf356040fd5e5de2da16c6b97700dff5
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-17 23:49:47 +0530

    creating multiple object of a class

---
+++ Oops/class/MultipleObject.py
@@ -0,0 +1,23 @@
+
+
+
+# creating multiple object of a class
+
+
+class employee:
+    employeeId = ""
+
+# create two objects of the Employee class
+
+employee1 = employee()  
+employee2 = employee()  
+
+# access attributes using employee1
+employee1.employeeId = 300
+print("Employeeid",employee1.employeeId)
+
+# access attributes using employee2
+employee2.employeeId = 400
+print("Employeeid",employee2.employeeId)
+
+
