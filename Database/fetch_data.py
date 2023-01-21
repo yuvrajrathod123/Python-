2ce10e056ac3ab29ef99e43e4721c918a31cab43
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-21 19:54:50 +0530

    fetching_data

--- Database/fetch_data.py
+++ Database/fetch_data.py
@@ -13,5 +13,44 @@ def showAllData():
     for i in result:
         print(i)
 
+# showAllData()
+
+# update data into database
+
+def UpdateData(name,email,city,sid):
+    conn = CreatConn()
+    cursor = conn.cursor()
+    args = (name,email,city,sid)
+    query = "update student set name=%s,email=%s,city=%s where sid=%s"
+    cursor.execute(query,args)
+    conn.commit()
+    print("data updated")
+
+showAllData()  #show database before updating
+
+sid = int(input("Enter your id:"))
+n = input("Enter your name:")
+e = input("Enter your email:")
+c = input("Enter your city")
+
+UpdateData(n,e,c,sid)   # for updteing
+
+showAllData() # for showing updated data
+
+# delete data fro, databse
+
+def  DeleteData(sid):
+    conn = CreatConn()
+    cursor = conn.cursor()
+    args = (sid)
+    query = "delete from student where sid=%s"
+    cursor.execute(query,args)
+    conn.commit()
+    print("data deleted")
+    conn.close()
+
 showAllData()
 
+sid = int(input("enter your id"))
+
+DeleteData(sid)
