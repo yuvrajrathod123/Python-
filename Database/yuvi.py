47c3e89bdc3d22181cf22fc097bda7f9faa15d23
yuvrajrathod123 <yuvirathod122@gmail.com>
2023-01-21 09:49:54 +0530

    create table in python

--- Database/yuvi.py
+++ Database/yuvi.py
@@ -3,3 +3,19 @@ import pymysql
 
 def CreatConn():
     return pymysql.connect(host="localhost",database="lernvern",user="root",password="Yuvraj@5587",port=3306)
+
+# create table
+
+def CreateTable():
+    conn = CreatConn()
+    cursor = conn.cursor()  #helps to execute query
+    query = "create table student(sid int primary key auto_increment, name varchar(50), email varchar(50),city varchar(45))" 
+
+    cursor.execute(query)
+    conn.commit()  #to save the data into database
+    print("table create")
+    conn.close()
+
+#calling create table method
+
+CreateTable()    
